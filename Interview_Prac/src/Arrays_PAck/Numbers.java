ffe878384f5d14e26b74e02d215cb6ca9437deec
Divyashree S R <divyashrisr1996@gmail.com>
2025-02-04 13:57:19 +0530

    "commit from eclipse"

---
+++ Interview_Prac/src/Arrays_PAck/Numbers.java
@@ -0,0 +1,20 @@
+package Arrays_PAck;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Reverse 2 number without 
+		int a=10,b=20,c = 0;
+		System.out.println("Before swap:"+"a:"+a+"b:"+b);
+		c=a+b;//30
+		a=c-a;//20
+		b=c-a;//10
+		
+		System.out.println("After swap:"+"a:"+a+"b:"+b);
+		
+		
+
+	}
+
+}
